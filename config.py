665ba93493f12b95d9b2124f1d2f9636555b0a82
Dimitrios Mourtzilas <158092133+Dimitrios-Mourtzilas1999@users.noreply.github.com>
2025-11-01 01:04:23 +0200

    Initial migrations. #4

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ import os
 class Config:
     """Base configuration."""
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+mysqldb://root:root@localhost/apotheke')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
     TESTING = False
+    FLASK_APP = os.environ.get('FLASK_APP','barcode_scanner')
 
 class DevelopmentConfig(Config):
     """Development environment configuration."""
@@ -16,13 +17,13 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Testing environment configuration."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # In-memory database for tests
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:root@localhost/apotheke'  # In-memory database for tests
 
 class ProductionConfig(Config):
     """Production environment configuration."""
     DEBUG = False
     # You might set a more secure database URI in production via environment variable
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://user:pass@localhost/dbname')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+mysqldb://root:root@localhost/apotheke')
 
 # Configuration dictionary for easy access
 config = {
