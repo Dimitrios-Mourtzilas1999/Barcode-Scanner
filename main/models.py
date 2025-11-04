8b0962740acd8689fb3a7e6190317add5d9722e7
Dimitrios Mourtzilas <158092133+Dimitrios-Mourtzilas1999@users.noreply.github.com>
2025-11-04 22:04:07 +0200

    Added roles table and relantionships. #6

--- main/models.py
+++ main/models.py
@@ -1,15 +1,17 @@
 from datetime import datetime
+from sqlalchemy import CheckConstraint, Enum
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from extensions import db
+from .extensions import db
 
 class User(db.Model,UserMixin):
     __tablename__ = 'users'
     __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String,nullable=False)
+    username = db.Column(db.String(10),nullable=False)
     password = db.Column(db.String(255),nullable=False)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'),nullable=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -35,3 +37,16 @@ class Product(db.Model):
 
     def __repr__(self):
         return f"Generated QR Code {self.id} - {self.bar}"
+    
+class Roles(db.Model):
+    __tablename__ = "roles"
+    __table_args__ = (
+        CheckConstraint(
+            "role IN ('admin', 'user')",
+            name='check_user_role'
+        ),
+        {'extend_existing': True}
+    )
+    id = db.Column(db.Integer,primary_key=True)
+    role = db.Column(Enum('admin', 'user', name='user_roles'), nullable=False)
+    user = db.relationship('User',backref='role',lazy=True)
